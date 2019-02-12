94d13a8684c0d4f6c12280ee96777bf7420894c5
Aksu <appsu-@hotmail.com>
2019-02-13 01:30:27 +0200

    Made edit functionality

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
@@ -28,16 +27,25 @@ public class BookController {
 		return "addbook";
 	}
 	
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@PostMapping("/save")
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
 	}
 	
-	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	@GetMapping("/delete/{id}")
     public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
         return "redirect:../booklist";
     }
+	
+	//Check ‘Add book’-functionality from the controller. In add functionality you added new book object to model but now you will
+	// add current book object to model. You also have to send current book id from the list page to controller (like you did in delete link).
+	
+	@GetMapping("/editbook/{id}")
+	public String editBook(@PathVariable("id") Long editId, Model model) {
+		model.addAttribute("book", repository.findById(editId));
+		return "editbook";
+	}
 	
 }
