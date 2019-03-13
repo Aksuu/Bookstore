c2363c8cbe16991ea46ab6dc767a014d8adf0f7a
Aksu <appsu-@hotmail.com>
2019-03-13 02:24:00 +0200

    Added email to user entity

--- src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
@@ -33,8 +33,8 @@ public class BookstoreApplication {
 			repository.save(new Book("Prince of Fools", "Mark Lawrence", "978-0-00-753156-1", 2015, 8.99, crepository.findByName("Fiction").get(0)));
 			repository.save(new Book("Harry Potter ja Azkabanin vanki", "J.K. Rowling", "978-951-31-1737-5", 1999, 19.99, crepository.findByName("Fantasy").get(0)));	
 			
-			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
-			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "user@email.com", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "admin@email.com", "ADMIN");
 			urepository.save(user1);
 			urepository.save(user2);
 			
--- src/main/java/hh/swd20/Bookstore/domain/User.java
+++ src/main/java/hh/swd20/Bookstore/domain/User.java
@@ -16,16 +16,20 @@ public class User {
 	@Column(name = "password", nullable = false)
 	private String passwordHash;
 	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
 	@Column(name = "role", nullable = false)
 	private String role;
 
 	public User() {
 	}
 
-	public User(String username, String passwordHash, String role) {
+	public User(String username, String passwordHash, String email, String role) {
 		super();
 		this.username = username;
 		this.passwordHash = passwordHash;
+		this.email = email;
 		this.role = role;
 	}
 
@@ -51,6 +55,14 @@ public class User {
 
 	public void setPasswordHash(String passwordHash) {
 		this.passwordHash = passwordHash;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getRole() {
